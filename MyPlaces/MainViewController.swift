2ad8972e8c97aaf3587341e245d7aa60ecda1e20
RamazanAlibek <ramazan.alibektyn@gmail.com>
2023-11-30 00:20:32 +0600

    places

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -9,10 +9,9 @@ import UIKit
 
 class MainViewController: UITableViewController {
     
-    let placeNames = [
-        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан", "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes", "Speak Easy", "Morris Pub", "Вкусные истории", "Классик", "Love&Life", "Шок", "Бочка"
-//        "My Room", "Smart Point", "Auezov library", "IITU library", "Burger King", "Salam Bro", "Halal Slice", "URBO"
-    ]
+   
+    
+    let places = Place.getPlaces()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,24 +21,20 @@ class MainViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return placeNames.count
+        return places.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
-        cell.nameLabel.text = placeNames[indexPath.row]
-        cell.imageOfPlace.image = UIImage(named: placeNames[indexPath.row])
+        cell.nameLabel.text = places[indexPath.row].name
+        cell.locationLabel.text = places[indexPath.row].location
+        cell.typeLabel.text = places[indexPath.row].type
+        cell.imageOfPlace.image = UIImage(named: places[indexPath.row].image)
         cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
         cell.imageOfPlace.clipsToBounds = true
 
         return cell
-    }
-    
-    // MARK: - TableView Delegate
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 85
     }
     
     
--- MyPlaces/PlaceModel.swift
+++ MyPlaces/PlaceModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+
+struct Place {
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+    
+    
+    
+    
+    static let placeNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан", "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes", "Speak Easy", "Morris Pub", "Вкусные истории", "Классик", "Love&Life", "Шок", "Бочка"
+//        "My Room", "Smart Point", "Auezov library", "IITU library", "Burger King", "Salam Bro", "Halal Slice", "URBO"
+    ]
+    
+    
+    static func getPlaces() -> [Place] {
+        
+        var places = [Place]()
+        
+        for place in placeNames {
+            places.append(Place(name: place, location: "Almaty", type: "Restoran", image: place))
+        }
+        
+        return places
+    }
+    
+    
+    
+
+    
+    
+}
